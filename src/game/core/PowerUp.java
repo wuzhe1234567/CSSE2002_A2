32ceabcdfca641251d2ca81ee2dd385efbdc57b3
wuzhe1234567 <s4909464@student.uq.edu.au>
2025-05-13 10:16:27 +1000

    Update PowerUp.java

--- src/game/core/PowerUp.java
+++ src/game/core/PowerUp.java
@@ -1,1 +1,29 @@
+package game.core;
 
+/**
+ * Represents a PowerUp in the game.
+ */
+public abstract class PowerUp extends ObjectWithPosition implements PowerUpEffect {
+
+    /**
+     * Creates a new PowerUp with the given coordinate.
+     *
+     * @param x the given x coordinate
+     * @param y the given y coordinate
+     */
+    public PowerUp(int x, int y) {
+        super(x, y);
+    }
+
+    /**
+     *Moves PowerUp downwards, once every 10 game ticks.
+     *
+     * @param tick the given game tick
+     */
+    @Override
+    public void tick(int tick) {
+        if (tick % 10 == 0) {
+            y++;
+        }
+    }
+}
