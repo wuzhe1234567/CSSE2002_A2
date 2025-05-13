4b63363f5e0f38a9851095e20068869ff50fbeb7
wuzhe1234567 <s4909464@student.uq.edu.au>
2025-05-13 10:16:41 +1000

    Update PowerUpEffect.java

--- src/game/core/PowerUpEffect.java
+++ src/game/core/PowerUpEffect.java
@@ -1,1 +1,14 @@
+package game.core;
+
+/**
+ * Represents the effect of a PowerUp in the game.
+ */
+public interface PowerUpEffect {
+    /**
+     * Applies the PowerUp's effect to the specified ship.
+     *
+     * @param ship the ship to apply the effect to
+     */
+    void applyEffect(Ship ship);
+}
 
