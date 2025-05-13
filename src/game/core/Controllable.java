e17e0c281c53e1c3c973688a06c6092038992b3a
wuzhe1234567 <s4909464@student.uq.edu.au>
2025-05-13 21:34:42 +1000

    Update Controllable.java

--- src/game/core/Controllable.java
+++ src/game/core/Controllable.java
@@ -21,15 +21,12 @@ public abstract class Controllable extends ObjectWithPosition {
 
     /**
      * Moves the Controllable by one in the direction given.
-     *
      * Throws BoundaryExceededException if the Controllable is attempting to move out-of-bounds.
      * A controllable is considered out-of-bounds if they are at:
      * x-coordinate >= GAME_WIDTH,
      * y-coordinate >= GAME_HEIGHT,
      * x-coordinate < 0, or
-     * y-coordinate < 0.
-     * Argument given to the exception is "Cannot move {up/down/left/right}. Out of bounds!" depending on the direction.
-     * Hint: game dimensions are stored in the model.
+     * y-coordinate < 0
      *
      * @param direction the given direction
      * @throws BoundaryExceededException if attempting to move out-of-bounds
@@ -38,12 +35,13 @@ public abstract class Controllable extends ObjectWithPosition {
         int newX = x;
         int newY = y;
         switch (direction) {
-            case UP ->    newY--;
-            case DOWN ->  newY++;
-            case LEFT ->  newX--;
+            case UP    -> newY--;
+            case DOWN  -> newY++;
+            case LEFT  -> newX--;
             case RIGHT -> newX++;
         }
         if (newX < 0 || newY < 0 || newX >= GAME_WIDTH || newY >= GAME_HEIGHT) {
+            // Exception message must match "Cannot move {up/down/left/right}. Out of bounds!"
             throw new BoundaryExceededException(
                     String.format("Cannot move %s. Out of bounds!", direction.name().toLowerCase())
             );
