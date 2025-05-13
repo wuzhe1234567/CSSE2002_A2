09c0144cab77725602c0d3f85df8b4d507f97c86
wuzhe1234567 <s4909464@student.uq.edu.au>
2025-05-13 10:14:20 +1000

    Update Asteroid.java

--- src/game/core/Asteroid.java
+++ src/game/core/Asteroid.java
@@ -1,1 +1,32 @@
+package game.core;
+
+import game.ui.ObjectGraphic;
+
+/**
+ * Represents an Asteroid in the game.
+ */
+public class Asteroid extends DescendingEnemy {
+
+    /**
+     * Creates a new Asteroid instance with coordinates x and y.
+     * @param x the x Coordinate
+     * @param y the y Coordinate
+     */
+    public Asteroid(int x, int y) {
+        super(x, y);
+    }
+
+    /**
+     * Returns a new ObjectGraphic with the appropriate text representation and image path.
+     *
+     * The text representation is "🌑".
+     * The image path is "assets/asteroid.png".
+     *
+     * @return the appropriate new ObjectGraphic
+     */
+    @Override
+    public ObjectGraphic render() {
+        return new ObjectGraphic("🌑", "assets/asteroid.png");
+    }
+}
 
