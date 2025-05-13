abe4f74d1130e26327cf7d64219ac0f99b541287
wuzhe1234567 <s4909464@student.uq.edu.au>
2025-05-13 11:13:21 +1000

    Update PlayerStatsTracker.java

--- src/game/achievements/PlayerStatsTracker.java
+++ src/game/achievements/PlayerStatsTracker.java
@@ -1,5 +1,30 @@
 package game.achievements;
 
 public class PlayerStatsTracker {
-    // 构造器 & stub 方法
+    private int shotsFired = 0;
+    private int shotsHit = 0;
+    private int enemiesDestroyed = 0;
+    private long startTime = System.currentTimeMillis();
+
+    public void recordAction(String action) {
+        // TODO: 根据 action 类型更新统计
+        if ("fire".equals(action)) shotsFired++;
+        if ("hit".equals(action)) shotsHit++;
+    }
+
+    public int getShotsFired() {
+        return shotsFired;
+    }
+
+    public int getShotsHit() {
+        return shotsHit;
+    }
+
+    public int getEnemiesDestroyed() {
+        return enemiesDestroyed;
+    }
+
+    public long getElapsedSeconds() {
+        return (System.currentTimeMillis() - startTime) / 1000;
+    }
 }
