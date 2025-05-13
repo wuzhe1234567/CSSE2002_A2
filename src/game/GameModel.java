023a4e5d597cbec82939adc3bd310601cc63e35f
wuzhe1234567 <s4909464@student.uq.edu.au>
2025-05-13 12:17:42 +1000

    Update GameModel.java

--- src/game/GameModel.java
+++ src/game/GameModel.java
@@ -59,6 +59,53 @@ public class GameModel {
     }
 
     /**
+     * Exposed for controller: updates state by delegating to updateGame
+     */
+    public void updateState(int tick) {
+        updateGame(tick);
+    }
+
+    /**
+     * Exposed for controller: processes player input
+     */
+    public void processInput(String key) {
+        if ("fire".equalsIgnoreCase(key)) {
+            fireBullet();
+            statsTracker.recordShotFired();
+        }
+        logger.log("Input: " + key);
+    }
+
+    /**
+     * Exposed for controller: returns current frame data
+     */
+    public Object getCurrentFrame() {
+        return new ArrayList<>(spaceObjects);
+    }
+
+    /**
+     * Exposed for controller: returns current score
+     */
+    public int getScore() {
+        return ship.getScore();
+    }
+
+    /**
+     * Exposed for controller: returns stats tracker
+     */
+    public PlayerStatsTracker getStats() {
+        return statsTracker;
+    }
+
+        this.logger = logger;
+        this.statsTracker = statsTracker;
+        this.spaceObjects = new ArrayList<>();
+        this.level = START_LEVEL;
+        this.spawnRate = START_SPAWN_RATE;
+        this.ship = new Ship();
+    }
+
+    /**
      * Returns the ship instance in the game.
      * @return the current ship instance.
      */
